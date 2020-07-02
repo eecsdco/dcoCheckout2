5df0e0d323e2c219591e5f789c23a8d5828f7eb4
Chris Puzzuoli <cpuzzuol@umich.edu>
2020-07-02 14:28:03 -0400

    added admins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,64 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411155625) do
+ActiveRecord::Schema.define(version: 2020_07_02_182423) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name",                null: false
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "description",         null: false
-    t.integer  "loan_length_seconds", null: false
+  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "uniqname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "notices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name",                     null: false
-    t.text     "text",       limit: 65535, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description", null: false
+    t.integer "loan_length_seconds", null: false
   end
 
-  create_table "offices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name",                     null: false
-    t.text     "address",    limit: 65535, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "notices", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "records", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.integer  "title_id",                      null: false
-    t.string   "borrower",                      null: false
-    t.text     "note",            limit: 65535
-    t.string   "agent",                         null: false
-    t.datetime "out",                           null: false
+  create_table "offices", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "records", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "title_id", null: false
+    t.string "borrower", null: false
+    t.text "note"
+    t.string "agent", null: false
+    t.datetime "out", null: false
     t.datetime "in"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "due"
     t.datetime "return_approved"
-    t.string   "option"
-    t.integer  "office_id"
-    t.index ["office_id"], name: "index_records_on_office_id", using: :btree
-    t.index ["title_id"], name: "index_records_on_title_id", using: :btree
+    t.string "option"
+    t.integer "office_id"
+    t.index ["office_id"], name: "index_records_on_office_id"
+    t.index ["title_id"], name: "index_records_on_title_id"
   end
 
-  create_table "titles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.integer  "category_id",                       null: false
-    t.string   "name",                              null: false
-    t.text     "description",         limit: 65535, null: false
-    t.integer  "n_available"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.integer  "loan_length_seconds"
-    t.integer  "notice_id"
-    t.integer  "office_id"
-    t.boolean  "enabled",                           null: false
-    t.text     "options_str",         limit: 65535
-    t.index ["category_id"], name: "index_titles_on_category_id", using: :btree
-    t.index ["notice_id"], name: "index_titles_on_notice_id", using: :btree
-    t.index ["office_id"], name: "index_titles_on_office_id", using: :btree
+  create_table "titles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "n_available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "loan_length_seconds"
+    t.integer "notice_id"
+    t.integer "office_id"
+    t.boolean "enabled", null: false
+    t.text "options_str"
+    t.index ["category_id"], name: "index_titles_on_category_id"
+    t.index ["notice_id"], name: "index_titles_on_notice_id"
+    t.index ["office_id"], name: "index_titles_on_office_id"
   end
 
   add_foreign_key "records", "offices"
-  add_foreign_key "titles", "notices"
 end
